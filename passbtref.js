34618682fac3a84b4f7c485deec3d3da5d1a3059
Minisha Shrestha <83584281+minisha-git@users.noreply.github.com>
2021-05-06 16:17:10 +0545

    passbyref

---
+++ passbtref.js
@@ -0,0 +1,11 @@
+function callByReference(varObj) { 
+  console.log("Inside Call by Reference Method"); 
+  varObj.a = 100; 
+  console.log(varObj); 
+} 
+let varObj = {a:1};
+console.log("Before Call by Reference Method"); 
+console.log(varObj);
+callByReference(varObj) 
+console.log("After Call by Reference Method"); 
+console.log(varObj);
